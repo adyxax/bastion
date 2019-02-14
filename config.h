a3f34674c6317930362c99f0fc40d835234843e5
Julien Dessaux <julien.dessaux@adyxax.org>
2019-02-14 20:51:41 +0100

    Moved code around

--- config.h
+++ config.h
@@ -14,11 +14,8 @@
 #define MYSQL_PASS "graou"
 #define MYSQL_DB "sshportal"
 
-#define SESSION_RECORDING  // comment this to deactivate
 #define LOG_FILENAME_FORMAT "./log/$d/$h/$u/$i.gz" // $d : date in iso format, $h : hostname, $u : username : $i session id
 #define LOG_FILENAME_MAX_LEN 255
 #define LOG_DIRECTORY_MODE S_IRUSR | S_IWUSR | S_IXUSR
-
-#define LIBSSH_VERBOSE_OUTPOUT // comment this to deactivate
 
 #endif
